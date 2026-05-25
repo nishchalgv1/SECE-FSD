4464a2eebafc5f2dbb58308fb558b3b5da468c32
nishchalgv1 <nischal.15gupta@gmail.com>
2026-05-25 16:06:17 +0530

    todo.ts

--- src/todo.ts
+++ src/todo.ts
@@ -2,25 +2,45 @@ interface Task{
     title: string, 
     id: number, 
     isCompleted: boolean,
-    priority: "Low" | "Medium" | "High"
+    priority?: "Low" | "Medium" | "High"
 }
 
 // create the empty array
 const tasks : Task[] = [];
 
-function addTask() : void{
-
+function addTask(task: Task) : void{
+    tasks.push(task);
     console.log("Task added successfully");
 }
 
 function showTasks() : void{
-
+    console.log("All Tasks are: ----\n");
+    console.log(tasks);
 }
 
-function deleteTask(id: number) : void{
-    // filter method in order to remove the task
-}
+// function deleteTask(id: number) : void{
+//     // filter method in order to remove the task
+// }
 
 // function searchTaskwithId() : Task{
 
 // }
+
+// function updateTask(){}
+
+const task:Task = {
+    title: "Learn Typescript",
+    id: 1,
+    isCompleted: true,
+}
+
+const task1:Task = {
+    title: "Learn Javascript",
+    id: 2,
+    isCompleted: true,
+}
+
+addTask(task);
+addTask(task1);
+
+showTasks();
